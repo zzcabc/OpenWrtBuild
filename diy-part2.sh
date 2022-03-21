842f3598802e98c0106fd4824002fe1f33330884
zhaocheng <zhaocheng990105@gmail.com>
2022-03-21 16:50:20 +0800

    尝试files大法

--- diy-part2.sh
+++ diy-part2.sh
@@ -20,15 +20,3 @@ CLASH_TUN_URL=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releas
 wget -qO- $OPENCLASH_MAIN_URL | tar xOvz > files/etc/openclash/core/clash
 wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
 chmod +x files/etc/openclash/core/clash*
-# openclash
-# DNS设置启动上游DNS，内置本机smartdns服务器
-# mkdir -p files/etc/config
-# curl -O https://raw.githubusercontent.com/zzcabc/OpenWrtBuild/main/openclash/openclash > files/etc/config/openclash
-
-# 使用配置的system
-# curl -O https://raw.githubusercontent.com/zzcabc/OpenWrtBuild/main/system/system > files/etc/config/system
-
-# 内置smartdns
-# mkdir -p files/etc/smartdns
-# curl -O https://raw.githubusercontent.com/zzcabc/OpenWrtBuild/main/smartdns/custom.conf > files/etc/smartdns/custom.conf
-# curl -O https://raw.githubusercontent.com/zzcabc/OpenWrtBuild/main/smartdns/smartdns > files/etc/config/smartdns
