426275122cd8088638daf4b3b217e41d2b2f2287
zhaocheng <zhaocheng990105@gmail.com>
2022-04-19 14:23:02 +0800

    update

--- diy-part2.sh
+++ diy-part2.sh
@@ -11,7 +11,7 @@
 #
 
 # Modify default IP
-sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
+# sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generate
 
 # 预置openclash内核
 mkdir -p files/etc/openclash/core
