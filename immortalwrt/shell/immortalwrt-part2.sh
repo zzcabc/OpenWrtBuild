3e13afd84213b6850d178a0a5106c54976a3de97
zhaocheng <zhaocheng990105@gmail.com>
2022-07-30 22:51:53 +0800

    openclash 内核下载地址更新

--- immortalwrt/shell/immortalwrt-part2.sh
+++ immortalwrt/shell/immortalwrt-part2.sh
@@ -15,10 +15,14 @@ sed -i 's/192.168.1.1/10.10.10.10/g' package/base-files/files/bin/config_generat
 
 # 预置openclash内核
 mkdir -p files/etc/openclash/core
-# dev内核
-# CLASH_DEV_URL=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/Clash | grep /clash-linux-amd64.tar.gz | awk -F '"' '{print $4}')
-# tun内核
-# CLASH_TUN_URL=$(curl -sL https://api.github.com/repos/vernesong/OpenClash/releases/tags/TUN-Premium | grep /clash-linux-amd64 | awk -F '"' '{print $4}' | head -n 1)
+
+# openclash 的 dev内核
+# CLASH_DEV_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/dev/clash-linux-amd64.tar.gz"
+# openclash 的 TUN内核
+# CLASH_TUN_URL=$(curl -fsSL https://api.github.com/repos/vernesong/OpenClash/contents/core-lateset/premium | grep download_url | grep clash-linux-amd64-2 | awk -F '"' '{print $4}' | head -n 1)
+# openclash 的 Meta内核版本
+# CLASH_META_URL="https://raw.githubusercontent.com/vernesong/OpenClash/master/core-lateset/meta/clash-linux-amd64.tar.gz"
+
 # d大的普核
 CLASH_DEV_URL=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/latest | grep /clash-linux-amd64-v1 | awk -F '"' '{print $4}')
 # d大的premium核
@@ -26,6 +30,11 @@ CLASH_TUN_URL=$(curl -sL https://api.github.com/repos/Dreamacro/clash/releases/t
 # meta核
 CLASH_META_URL=$(curl -sL https://api.github.com/repos/MetaCubeX/Clash.Meta/releases/tags/Prerelease-Alpha | grep /Clash.Meta-linux-amd64-compatible-alpha | awk -F '"' '{print $4}' | head -n 1)
 # 下载clash内核
+# openclash 的 内核解压
+# wget -qO- $CLASH_DEV_URL | tar xOvz > files/etc/openclash/core/clash
+# wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
+# wget -qO- $CLASH_META_URL | tar xOvz > files/etc/openclash/core/clash_meta
+
 wget -qO- $CLASH_DEV_URL | gunzip -c > files/etc/openclash/core/clash
 wget -qO- $CLASH_TUN_URL | gunzip -c > files/etc/openclash/core/clash_tun
 wget -qO- $CLASH_META_URL | gunzip -c > files/etc/openclash/core/clash_meta
@@ -38,12 +47,12 @@ COUNTRY_LITE_URL=https://raw.githubusercontent.com/alecthw/mmdb_china_ip_list/re
 wget -qO- $COUNTRY_LITE_URL > files/etc/openclash/Country.mmdb
 # wget -qO- $COUNTRY_FULL_URL > files/etc/openclash/Country.mmdb
 
-chmod +rw files/etc/openclash/Country.mmdb
+chmod +r files/etc/openclash/Country.mmdb
 
 # meta可能要GeoIP.dat 和 GeoSite.dat
-GEOIP_URL=https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat
-GEOSITE_URL=https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat
+GEOIP_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geoip.dat"
+GEOSITE_URL="https://github.com/Loyalsoldier/v2ray-rules-dat/releases/latest/download/geosite.dat"
 wget -qO- $GEOIP_URL > files/etc/openclash/GeoIP.dat
 wget -qO- $GEOSITE_URL > files/etc/openclash/GeoSite.dat
 
-chmod +rw files/etc/openclash/*.dat
+chmod +r files/etc/openclash/*.dat
